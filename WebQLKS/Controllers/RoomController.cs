4a9daaafdbb440ce7dcbb8bd06cf3a683070e987
NguyenDucTinNew <93468105+HuyLieu1603@users.noreply.github.com>
2024-06-22 16:34:31 +0700

    Add project files.

---
+++ WebQLKS/Controllers/RoomController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebQLKS.Models;
+
+namespace WebQLKS.Controllers
+{
+    public class RoomController : Controller
+    {
+        DAQLKSEntities1 database = new DAQLKSEntities1();
+        // GET: Room
+        public ActionResult Index()
+        {
+            var room = database.tbl_Phong.ToList();
+            return View(room);
+        }
+    }
+}
