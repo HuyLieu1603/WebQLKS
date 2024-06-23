a1ad3bc16f6754e40889bba30bde20d03aab3d8a
NguyenDucTinNew <93468105+HuyLieu1603@users.noreply.github.com>
2024-06-23 19:59:40 +0700

    +Chỉnh sửa layout +Thêm trang chủ

--- WebQLKS/App_Start/BundleConfig.cs
+++ WebQLKS/App_Start/BundleConfig.cs
@@ -19,12 +19,14 @@ namespace WebQLKS
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            bundles.Add(new Bundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/Site.css"));
+
+            BundleTable.EnableOptimizations = false;
         }
     }
 }
