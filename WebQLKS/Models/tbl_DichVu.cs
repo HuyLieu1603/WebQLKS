75147037898627a23c06af38d7d003c7a2823d3a
Trịnh Hữu Gia Bảo <22dh110327@st.huflit.edu.vn>
2024-07-04 18:25:04 +0700

    update

--- WebQLKS/Models/tbl_DichVu.cs
+++ WebQLKS/Models/tbl_DichVu.cs
@@ -24,6 +24,7 @@ namespace WebQLKS.Models
         public string TenDV { get; set; }
         public Nullable<decimal> DonGia { get; set; }
         public string MaLoaiDV { get; set; }
+        public string img { get; set; }
         public string MoTa { get; set; }
     
         public virtual tbl_LoaiDichVu tbl_LoaiDichVu { get; set; }
