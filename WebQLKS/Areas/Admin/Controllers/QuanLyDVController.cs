a4647e4c856cc5b51b5c9b92740c60f3536eef9d
NguyenDucTinNew <93468105+HuyLieu1603@users.noreply.github.com>
2024-07-06 14:10:48 +0700

    update

---
+++ WebQLKS/Areas/Admin/Controllers/QuanLyDVController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebQLKS.Models;
+
+namespace WebQLKS.Areas.Admin.Controllers
+{
+    public class QuanLyDVController : Controller
+    {
+        DAQLKSEntities db = new DAQLKSEntities();
+        // GET: QuanLyDV
+        public ActionResult DanhSachPhong()
+        {
+            var room = db.tbl_Phong.ToList();
+            return View(room);
+        }
+        //Thêm Phòng
+        [HttpGet]
+        public ActionResult ThemPhong()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult ThemPhong()
+        {
+
+            return View(); 
+        }
+
+        public ActionResult DanhMucPhong()
+        {
+            var roomType = db.tbl_LoaiPhong.ToList();
+            return View(roomType);
+        }
+
+        public ActionResult ThucDon()
+        {
+            var menu = db.tbl_DichVu.Where(m => m.MaLoaiDV == "DV02").ToList();
+            return View(menu);
+        }
+        public ActionResult Spa()
+        {
+            var spa = db.tbl_DichVu.Where(s => s.MaLoaiDV == "DV03").ToList();
+            return View(spa);
+        }
+        public ActionResult YeuCauDonDep()
+        {
+            var lstRequest = db.tbl_Phong.Where(r => r.MaTrangThai == "TT03").ToList();
+            return View(lstRequest);
+        }
+    }
+}
